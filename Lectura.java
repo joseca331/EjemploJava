27015448bc0bd511632ee6643a49bdad16ded8fd
joseca331 <blanco.gonzalez.jose.carlos@iescamas.es>
2025-09-18 19:22:18 +0200

    Add files via upload

---
+++ Lectura.java
@@ -0,0 +1,21 @@
+package holaMundo;
+import java.util.Scanner;
+
+public class Lectura {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduce tu nombre: ");
+		
+		String nombre = sc.nextLine();
+		
+		System.out.println("Hola " + nombre);
+		
+		
+	}
+
+}
---
+++ TipoDatos.java
@@ -0,0 +1,27 @@
+package holaMundo;
+
+public class TipoDatos {
+
+	public static void main(String[] args) {
+
+		int numero = 100;
+		
+		float numero_decimal = 1.1F;
+		double numero_decimal2 = 1.1;
+		
+		boolean logico = false;
+		
+		String palabra = "Tomante";
+		
+		char letra = '1';
+		
+		
+		System.out.println("numero" + 100);
+		
+		
+		
+		
+		
+	}
+
+}
