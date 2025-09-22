383171ed588757ef606be6a63515c6f6e90b60b2
joseca331 <blanco.gonzalez.jose.carlos@iescamas.es>
2025-09-22 18:03:56 +0200

    Add files via upload

--- Actividad12.java
+++ Actividad12.java
@@ -5,13 +5,15 @@ public class Actividad12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Introduce un número: ");
-		double numero = scanner.nextDouble();
+		int numero = sc.nextInt();
 
-		double valorAbsoluto = Math.abs(numero);
+		int valorAbsoluto = Math.abs(numero);
 
 		        System.out.println("El valor absoluto es: " + valorAbsoluto);
 		    }
 		}
+
+
